d221f4c6a4b0f0bca71e9104f6ea38bee85247e7
蒙明宇 <mmy6076@outlook.com>
2022-01-06 21:08:04 +0800

    feat：post and detail

---
+++ src/Blog.App/Pages/Posts/Posts.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blog.App.Pages.Posts;
+
+public partial class Posts
+{
+    [Inject]
+    public NavigationManager Nav { get; set; } = default!;
+
+    private void NavigateToDetails(Guid id)
+    {
+        Nav.NavigateTo($"post/details/{id}");
+    }
+}
+
