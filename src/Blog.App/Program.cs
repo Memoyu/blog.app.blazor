0c1786dee0c5a77fd1553b6d872c654e8b068011
蒙明宇 <mmy6076@outlook.com>
2022-02-21 18:07:42 +0800

    fix：test

--- src/Blog.App/Program.cs
+++ src/Blog.App/Program.cs
@@ -7,7 +7,6 @@ var Configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
 builder.Services.AddMasaBlazor(builder =>
 {
     builder.UseTheme(option =>
@@ -31,8 +30,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
